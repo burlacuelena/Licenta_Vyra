b22693392282f06c56f9aabc3718b5f41b1c0b7d
burlacuelena <elena.burlacu@student.usv.ro>
2026-02-18 21:54:23 +0200

    Create SignIn/SignUp

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Funcția pentru Sign In (deja o ai)
+    public void GoToSignIn()
+    {
+        SceneManager.LoadScene("SignInScene");
+    }
+
+    // Funcția NOUĂ pentru Sign Up
+    public void GoToSignUp()
+    {
+        SceneManager.LoadScene("SignUpScene");
+    }
+}
---
+++ Assets/Scripts/ToggleVisibility.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI; // Adăugat pentru a manipula iconița dacă e nevoie
+
+public class PasswordVisibility : MonoBehaviour
+{
+    public TMP_InputField passwordField;
+    public GameObject eyeIconObject; // Obiectul cu iconița ochiului
+
+    void Start()
+    {
+        // Ne asigurăm că la început parola este mereu ascunsă
+        passwordField.contentType = TMP_InputField.ContentType.Password;
+        passwordField.ForceLabelUpdate();
+    }
+
+    public void TogglePassword()
+    {
+        if (passwordField.contentType == TMP_InputField.ContentType.Password)
+        {
+            // O facem vizibilă
+            passwordField.contentType = TMP_InputField.ContentType.Standard;
+
+            // Opțional: Schimbăm aspectul ochiului (ex: îl facem mai transparent)
+            if (eyeIconObject != null) eyeIconObject.SetActive(true);
+        }
+        else
+        {
+            // O ascundem cu steluțe
+            passwordField.contentType = TMP_InputField.ContentType.Password;
+
+            // Opțional: Schimbăm aspectul ochiului
+            // if(eyeIconObject != null) eyeIconObject.SetActive(false);
+        }
+
+        passwordField.ForceLabelUpdate();
+    }
+}
